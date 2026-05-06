97146aca0cf1003e01bc59c4803bd0e403a75ac9
Roger Chappel <miscanalysis@gmail.com>
2026-05-06 18:34:59 +1000

    feat: export public library API

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export { validateInboundPath, validateOutboundPath } from './core/validate.js';
+export { replayInboundPath } from './core/replay.js';
+export { inspectPath, inspectSuite } from './core/inspect.js';
+export { validateInboundEvent } from './core/inbound.js';
+export { validateOutboundAction } from './core/outbound.js';
+export { validateMessage } from './core/message.js';
+export { SAFETY } from './core/types.js';
